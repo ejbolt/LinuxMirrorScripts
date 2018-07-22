2ac7ca78790a630fbfa366e9937fd8c59bb691dc
Evan Bolt <boltevan7@gmail.com>
2018-07-21 23:05:40 -0500

    Update ubuntu-clean-logs.sh
    formatting

--- ubuntu/ubuntu-clean-logs.sh
+++ ubuntu/ubuntu-clean-logs.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-cd /home/archvsync/log/ubuntu
+cd <Path to Log Directory>/ubuntu
 ls -t | tail -n +101 | xargs rm --
 
-cd /home/archvsync/log/ubuntu-cdimage
+cd <Path to Log Directory>/ubuntu-cdimage
 ls -t | tail -n +21 | xargs rm --
 
-cd /home/archvsync/log/ubuntu-releases
+cd <Path to Log Directory>/ubuntu-releases
 ls -t | tail -n +21 | xargs rm --
 
